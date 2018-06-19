f9dabd01e1e4b54ae20aa0eda889cd7dc6dd6e78
vciobanu <vciobanu@paypal.com>
2018-06-19 11:41:58 +0200

    initial commit

---
+++ src/main/java/com/vciobanu/filteringmatches/FilteringMatchesApplication.java
@@ -0,0 +1,12 @@
+package com.vciobanu.filteringmatches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringMatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilteringMatchesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vciobanu/filteringmatches/FilteringMatchesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vciobanu.filteringmatches;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilteringMatchesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
